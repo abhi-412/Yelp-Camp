4df806b7bcb87f3d31253cdb910ce380f16dc65a
abhi-412 <yabhay@iiitmanipur.ac.in>
2023-06-19 06:06:46 +0530

    Initial commit

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const user = require('../models/user');
+const User = require('../models/user');
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req,res)=>{
+    req.flash('success', 'Welcome back to Yelp-Camp');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+   delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) => {
+    req.logout(user);
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
